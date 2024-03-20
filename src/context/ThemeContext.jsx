c1d2cc5599d5e253c9e37c3c2f220a9b066880a8
Akshay Kamble <123529533+akshaykamble04@users.noreply.github.com>
2024-03-20 19:31:42 +0530

    theme context updated

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,6 +3,10 @@ import { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
+const getFromLocalStorage = () => {
+    const value = localStorage.getItem("theme");
+}
+
 export const ThemeContextProvider = ({ children }) => {
     const [theme, setTheme] = useState("light");
     return <ThemeContext.Provider>{children}</ThemeContext.Provider>
