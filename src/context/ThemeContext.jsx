379418560420c8a6d754a0f85cb84805d218f351
Akshay Kamble <123529533+akshaykamble04@users.noreply.github.com>
2024-03-20 22:33:51 +0530

    dark mode and light mode completed using context provider

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,13 +1,35 @@
-import { createContext, useState } from "react";
+"use client";
+import { createContext, useEffect, useState } from "react";
+
 
 
 export const ThemeContext = createContext();
 
 const getFromLocalStorage = () => {
-    const value = localStorage.getItem("theme");
+
+    if (typeof window !== "undefined") {
+        const value = localStorage.getItem("theme");
+        return value || "light";
+    }
 }
 
+
 export const ThemeContextProvider = ({ children }) => {
-    const [theme, setTheme] = useState("light");
-    return <ThemeContext.Provider>{children}</ThemeContext.Provider>
+    const [theme, setTheme] = useState(() => {
+        return getFromLocalStorage();
+    });
+
+    const toggle = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    };
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggle }}>
+            {children}
+        </ThemeContext.Provider>
+    );
 }
